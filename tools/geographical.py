5ee4ce7072478ae0054e6831af897eb1553ba037
Andrew Walker <a.walker@leeds.ac.uk>
2015-09-10 09:46:01 +0100

    Add a tool to calculate azimuth and distance
    This is from my "CoMITAC flow" stuff, and even
    comes with tests.

---
+++ tools/geographical.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python
+#
+# Key primitives for cartesian to geographical 
+#  coordinate transforms
+#
+# Copyright (C) Andrew Walker, 2010, 2011
+#               <andrew.walker@bristol.ac.uk>
+import math as m
+
+def geog2cart(r, lat, lon):
+    """Converts from geograpical to 
+       cartesian coordinates. Input lat
+       and lon in degrees (lat: 90 -> 0 -> -90 
+       north to south, lon: 0 -> 360 or 0 -> 
+       90 -> 180 -> -90 -> 0 E -> W). Output in 
+       units of r."""
+
+    theta = m.radians(90.0 - lat)
+    if (lon < 0.0): 
+        lon = 360.0 + lon # -ve lon is degrees W of grenwich
+    phi = m.radians(lon)
+
+    return (sper2cart(r, phi, theta))
+
+def cart2geog(x, y, z):
+    """Converts from our cartesian system (X3 to N pole,
+       X1 to 0 E 0 N, X2 to 90 E 0 N) to geographical 
+       coordinates (r, lat, lon). Radius units is the same
+       as the cartesian units system (typically km)."""
+    (r, theta, phi) = cart2sper(x, y, z)
+    lon = m.degrees(phi)
+    lat = (90.0 - m.degrees(theta))
+    return(r, lat, lon)
+
+def cart2sper(x, y, z):
+    """Converts from cartesian to spherical coordiates,
+       angular results in radians, radial in units of 
+       cart system. Theta is the angle between the point
+       and the +ve Z-axis, phi is the angle between the 
+       XZ plane and the point measured paralell to the XY
+       plane."""
+    r = m.sqrt(x**2 + y**2 + z**2)
+    # NB arctan2(a, b) is arctan(a/b) sorting out the quadrent
+    phi = m.atan2(y, x)
+    theta = m.acos(z/r)
+    return(r, theta, phi)
+
+def sper2cart (r, phi, theta):
+    """Converts from sperical coordinates 
+       to cartesian coordinates. Input theta
+       and phi are in radians"""
+
+    x = r * m.cos(phi) * m.sin(theta)
+    y = r * m.sin(phi) * m.sin(theta)
+    z = r * m.cos(theta)
+    return (x, y, z)
+
+def geog2str(r, lat, lon):
+    return "r = " + str(r) + " lat = " + str(lat) + " lon = " + str(lon)
+
+def cart2str(x, y, z):
+    return "x = " + str(x) + " y = " + str(y) + " z = " + str(z)
+
+def vincenty(lat1, lon1, lat2, lon2, 
+             r_major=6378.1370, r_minor=6356.752314, r_sphere=None):
+    """
+    Calulates the distance and direction between two points on an 
+    ellipsoid using Vincenty's inverse approach (T. Vincenty 1975,
+    "Direct and inverse solutions of geodesics on the ellipsoid with
+    application of nested equations" Survey Review XXII pp.88-93).
+
+    Arguments are the lattitude and longitude of the two points (in 
+    degrees) and, optionally, the semi-major and semi-minor radius of the
+    ellipsoid or the radius of a sphere (if polar flattening is ignored). 
+    These default to the WGS 84 ellipsoid. Changing the units of the 
+    radii changes the units of the resultant distance, but note that the
+    other arguments shouls always be in degrees.
+
+    A length three tuple is returned. The first element is the distance 
+    between the two points, the second and third are the azimuths of the 
+    geodesic in degrees clockwise from north. 
+    """
+
+    lat1 = m.radians(lat1)
+    lat2 = m.radians(lat2)
+    lon1 = m.radians(lon1)
+    lon2 = m.radians(lon2)
+
+    if (r_sphere is not None):
+        r_major = r_sphere
+        r_minor = r_sphere
+        f = 0.0
+    else:
+        f = (r_major-r_minor)/r_major
+
+    U1 = m.atan((1.0-f) * m.tan(lat1))
+    U2 = m.atan((1.0-f) * m.tan(lat2))
+    L = lon2 - lon1
+
+    epsilon = 1E-12 # Accuracy (10E-12 -> ~ 0.06mm)
+    max_iter = 500 
+    lam = L
+
+    cU1 = m.cos(U1)
+    cU2 = m.cos(U2)
+    sU1 = m.sin(U1)
+    sU2 = m.sin(U2)
+
+    for i in range(max_iter):
+        lam_old = lam
+        sLam = m.sin(lam)
+        cLam = m.cos(lam)
+        sin_sig = m.sqrt((cU2*sLam)**2 + (cU1*sU2 - sU1*cU2*cLam)**2)
+        cos_sig = sU1*sU2 + cU1*cU2*cLam
+        sig = m.atan2(sin_sig,cos_sig)
+        sin_alp = (cU1*cU2*sLam) / sin_sig
+        cos2_alp = 1.0 - sin_alp**2
+        if (cos2_alp == 0.0): 
+            # equitorial line
+            cos_2sigm = 100
+            C = 0.0
+        else:
+            cos_2sigm = cos_sig - (2.0*sU1*sU2)/cos2_alp
+            C = f/16.0 * cos2_alp * (4.0 + f*(4.0-3.0*cos2_alp))
+        lam = L + (1.0 - C) * f * sin_alp * \
+            (sig + C * sin_sig * (cos_2sigm + C * cos_sig * \
+            (-1.0 + 2.0 * cos_2sigm**2)))
+        if ((m.fabs(lam - lam_old)) <= epsilon):
+            # Found a solution in i iters...
+            break
+
+    usq = cos2_alp * ((r_major**2 - r_minor**2) / r_minor**2)
+    A = 1 + usq/16384 * (4096 + usq*(-768 + usq*(320 - 175*usq)))
+    B = usq/1024 * (256 + usq*(-128 + usq*(74 - 47*usq)))
+    del_sig = B * sin_sig * (cos_2sigm + 0.25*B*(cos_sig*( \
+        -1 + 2*cos_2sigm**2) - (1.0/6.0)*B*cos_2sigm * ( \
+        -3 + 4*sin_sig**2) * (-3 + 4 * cos_2sigm**2)))
+    s = r_minor * A * (sig - del_sig)
+    alp1 = m.atan2(cU2*m.sin(lam),(cU1*sU2-sU1*cU2*m.cos(lam)))
+    alp2 = m.atan2(cU1*m.sin(lam),(cU1*sU2*m.cos(lam)-sU1*cU2))
+
+    return (s, m.degrees(alp1), m.degrees(alp2))
+
+def dms2dec(degs,mins,secs):
+        """Converts angle given in degrees, mins and secs to
+           angle in decimal degrees"""
+        return (degs + mins/60.0 + secs/3600.0)
+
+
+if __name__ == "__main__":
+    print "Testing co-ordinate conversion"
+
+    print "Geographical point:"
+    print geog2str(1000, 0.0, 0.0)
+    print "Cartesian point:"
+    (x, y, z) = geog2cart(1000, 0.0, 0.0)
+    print cart2str(x, y, z)
---
+++ tools/tests/test_grographical.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+import geographical as gt
+import unittest
+import numpy.testing as npt # This has support for array-like test comparisons
+import random
+
+class TestGeographicalFunctions(unittest.TestCase):
+
+    def setUp(self):
+        # These are all "known points" as (x, y, z) or (r, lat, lon)
+        self.north_pole_geog = (6378.0, 90.0, 0.0)
+        self.north_pole_cart = (0.0, 0.0, 6378.0)
+        self.south_pole_geog = (6378.0, -90.0, 0.0)
+        self.south_pole_cart = (0.0, 0.0, -6378.0)
+        self.x0_geog = (6378.0, 0.0, 0.0)
+        self.x0_cart = (6378.0, 0.0, 0.0)
+        self.x1_geog = (6378.0, 0.0, 180.0)
+        self.x1_cart = (-6378.0, 0.0, 0.0)
+        self.y0_geog = (6378.0, 0.0, 90.0)
+        self.y0_cart = (0.0, 6378.0, 0.0)
+        self.y1_geog = (6378.0, 0.0, -90.0)
+        self.y1_cart = (0.0, -6378.0, 0.0)
+
+    def test_cart2geog_elements(self):
+        self.assertEqual(gt.cart2geog(self.north_pole_cart[0],self.north_pole_cart[1],
+                           self.north_pole_cart[2]), self.north_pole_geog)
+        self.assertEqual(gt.cart2geog(self.south_pole_cart[0],self.south_pole_cart[1],
+                           self.south_pole_cart[2]), self.south_pole_geog)
+        self.assertEqual(gt.cart2geog(self.x0_cart[0],self.x0_cart[1],self.x0_cart[2]), self.x0_geog)
+        self.assertEqual(gt.cart2geog(self.x1_cart[0],self.x1_cart[1],self.x1_cart[2]), self.x1_geog)
+        self.assertEqual(gt.cart2geog(self.y0_cart[0],self.y0_cart[1],self.y0_cart[2]), self.y0_geog)
+        self.assertEqual(gt.cart2geog(self.y1_cart[0],self.y1_cart[1],self.y1_cart[2]), self.y1_geog)
+
+    def test_geog2cart_elements(self):
+        npt.assert_almost_equal(gt.geog2cart(self.north_pole_geog[0],self.north_pole_geog[1],self.north_pole_geog[2]), self.north_pole_cart)
+        npt.assert_almost_equal(gt.geog2cart(self.south_pole_geog[0],self.south_pole_geog[1],self.south_pole_geog[2]), self.south_pole_cart)
+        npt.assert_almost_equal(gt.geog2cart(self.x0_geog[0],self.x0_geog[1],self.x0_geog[2]), self.x0_cart)
+        npt.assert_almost_equal(gt.geog2cart(self.x1_geog[0],self.x1_geog[1],self.x1_geog[2]), self.x1_cart)
+        npt.assert_almost_equal(gt.geog2cart(self.y0_geog[0],self.y0_geog[1],self.y0_geog[2]), self.y0_cart)
+        npt.assert_almost_equal(gt.geog2cart(self.y1_geog[0],self.y1_geog[1],self.y1_geog[2]), self.y1_cart)
+
+    def test_geog2cart2geog_fuse(self):
+        random.seed(987.987)
+        for i in range(1000):
+            r = random.uniform(0.0, 1000) 
+            lat = random.uniform(-90,90)
+            lon = random.uniform(-180, 180)
+            (x, y, z) = gt.geog2cart(r, lat, lon)
+            npt.assert_almost_equal(gt.cart2geog(x, y, z), (r, lat, lon))
+        
+    def test_cart2geog2cart_fuse(self):
+        random.seed(987.987)
+        for i in range(1000):
+            x = random.uniform(-1000, 1000) 
+            y = random.uniform(-1000,1000)
+            z = random.uniform(-1000, 1000)
+            (r, lat, lon) = gt.cart2geog(x, y, z)
+            npt.assert_almost_equal(gt.geog2cart(r, lat, lon), (x, y, z))
+
+class TestVincentyFunctions(unittest.TestCase):
+   
+    # Table II of Vincenty's paper (T. Vincenty 1975, "Direct
+    # and inverse solutions of geodesics on the ellipsoid with
+    # application of nested equations" Survey Review XXII 
+    # pp.88-93) has five test examples for the forward and inverse
+    # problem (with results rounded to 0.00001 seconds of arc and 
+    # 1 mm). The inverse versions of these are implemented here. Note
+    # the non-standard (old) ellipsoid usage.
+
+    #def test_vincenty_inverse_tabulated_a(self):
+    #    correct_result = (14110.526170, gt.dms2dec(96.0,36.0,8.79960), 
+    #                      gt.dms2dec(137.0,52.0,22.01454))
+    #    lat1 = gt.dms2dec(55.0,45.0,0.0)
+    #    lat2 = gt.dms2dec(-33.0,26.0,0.0)
+    #    lon2 = gt.dms2dec(108.0,13.0,0.0)
+    #    npt.assert_almost_equal(gt.vincenty(lat1,0.0,lat2,lon2,
+    #                  r_major=6377.397155,r_minor=6356.078963),correct_result)
+
+    def test_vincenty_inverse_tabulated_b(self):
+        correct_result = (4085.966703, gt.dms2dec(95.0,27.0,59.63089), 
+                          gt.dms2dec(118,5.0,58.96161))
+        lat1 = gt.dms2dec(37.0,19.0,54.95367)
+        lat2 = gt.dms2dec(26.0,7.0,42.83946)
+        lon2 = gt.dms2dec(41.0,28.0,35.50729)
+        npt.assert_almost_equal(gt.vincenty(lat1,0.0,lat2,lon2,
+                  r_major=6378.388000,r_minor=6356.911946),correct_result,6)
+
+    def test_vincenty_inverse_tabulated_c(self):
+        correct_result = (8084.823839, gt.dms2dec(15.0,44.0,23.74850), 
+                          gt.dms2dec(144.0,55.0,39.92147))
+        lat1 = gt.dms2dec(35.0,16.0,11.24862)
+        lat2 = gt.dms2dec(67.0,22.0,14.77638)
+        lon2 = gt.dms2dec(137.0,47.0,28.31435)
+        npt.assert_almost_equal(gt.vincenty(lat1,0.0,lat2,lon2,
+                  r_major=6378.388000,r_minor=6356.911946),correct_result,6)
+
+    # FIXME
+    # Skip tabulated data d - very sensive to lattitude...
+
+    def test_vincenty_inverse_tabulated_e(self):
+        correct_result = (19780.006558, gt.dms2dec(4.0,59.0,59.99995), 
+                          gt.dms2dec(174.0,59.0,59.88481))
+        lat1 = gt.dms2dec(1.0,0.0,0.0)
+        lat2 = gt.dms2dec(1.0,1.0,15.18952)
+        lon2 = gt.dms2dec(179.0,46.0,17.84244)
+        npt.assert_almost_equal(gt.vincenty(lat1,0.0,lat2,lon2,
+                  r_major=6378.388000,r_minor=6356.911946),correct_result,6)
+
+if __name__ == '__main__':
+    unittest.main()
+
